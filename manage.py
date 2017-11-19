a5876a1c1085dc75221753dfa464b62a9fcfaeb3
zihaow95 <32386524+zihaow95@users.noreply.github.com>
2017-11-19 15:42:50 +0000

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@ import os
 from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from app.models import Developer, Integration, Channel, User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Developer = Developer, Integration=Integration, Channel=Channel, User=User)
 
 manager.add_command("shell",Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
