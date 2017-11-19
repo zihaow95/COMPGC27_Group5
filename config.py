b952ca4f61a689b04acb369d5a81027dc73cc888
zihaow95 <32386524+zihaow95@users.noreply.github.com>
2017-11-19 01:49:02 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,33 @@
-class Comfig(object):
-	pass
+import os
 
-class ProdConfig(object):
-	pass
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class DevConfig(object):
-	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+class config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret string'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'test')
+
+class ProductionConfig(config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
-from flask_script import Manager, Server
-from main import app, db, Annual_Full_2016
+import os
+from app import create_app, db
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
-manager.add_command("server", Server())
-@manager.shell
 def make_shell_context():
-	return dict(app=app, db=db, Annual_Full_2016=Annual_Full_2016)
+    return dict(app=app, db=db)
 
-if __name__ == "__main__":
-	manager.run()
+manager.add_command("shell",Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
