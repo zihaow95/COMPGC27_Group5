78930dd8af65ab71208707a0d63cc51dc6218b9e
zihaow95 <32386524+zihaow95@users.noreply.github.com>
2017-12-08 17:27:36 +0000

    Add files via upload

--- pokemon/views.py
+++ pokemon/views.py
@@ -26,66 +26,176 @@ def querypage():
     if flask.request.method == 'GET':
         result0 = {}
         return render_template("querypage.html", result0=result0)
-
+# user's team
+    # query 1st pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('query1', None) == "query":
         pokemon_name = flask.request.form["Pokemon_name1"]
         name = validName(pokemon_name)
         if name != 0:
             result1 = queryPokemon(name)
             result[0] = result1
-            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2])
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
         else:
-            return render_template("querypage.html", result2= result[1], result3=result[2], warning1="Please enter the correct name.")
-
+            return render_template("querypage.html", result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning1="Please enter the correct name.")
+    # query 2nd pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('query2', None) == 'query':
         pokemon_name = flask.request.form["Pokemon_name2"]
         name = validName(pokemon_name)
         if name != 0:
             result2 = queryPokemon(name)
             result[1]=result2
-            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2])
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
         else:
-            return render_template("querypage.html", result1=result[0], result3=result[2], warning2="Please enter the correct name.")
-
+            return render_template("querypage.html", result1=result[0], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning2="Please enter the correct name.")
+    # query 3rd pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('query3', None) == 'query':
         pokemon_name = flask.request.form["Pokemon_name3"]
         name = validName(pokemon_name)
         if name != 0:
             result3 = queryPokemon(name)
             result[2] = result3
-            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2])
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
         else:
-            return render_template("querypage.html", result1=result[0], result2= result[1],warning3="Please enter the correct name.")
+            return render_template("querypage.html", result1=result[0], result2= result[1],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning3="Please enter the correct name.")
 
+    # add 1st pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('add1', None) == 'add':
         pokemon_name = flask.request.form["Pokemon_name1"]
         name = validName(pokemon_name)
         if name != 0:
             name_list[0] = (pokemon_name)
             return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
                                    addation1="{} added".format(pokemon_name))
         else:
-            return render_template("querypage.html",  result2= result[1], result3=result[2],warning1="Please enter the correct name.")
-
+            return render_template("querypage.html", result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning1="Please enter the correct name.")
+    # add 2nd pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('add2', None) == 'add':
         pokemon_name = flask.request.form["Pokemon_name2"]
         name = validName(pokemon_name)
         if name != 0:
             name_list[1] = (pokemon_name)
             return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
                                    addation2="{} added".format(pokemon_name))
         else:
-            return render_template("querypage.html", result1=result[0],result3=result[2],warning2="Please enter the correct name.")
-
+            return render_template("querypage.html", result1=result[0], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning2="Please enter the correct name.")
+    # add 3rd pokemon
     elif flask.request.method == 'POST' and flask.request.form.get('add3', None) == 'add':
         pokemon_name = flask.request.form["Pokemon_name3"]
         name = validName(pokemon_name)
         if name != 0:
             name_list[2] = (pokemon_name)
             return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
                                    addation3="{} added".format(pokemon_name))
         else:
-            return render_template("querypage.html", result2= result[1], result3=result[2],warning3="Please enter the correct name.")
+            return render_template("querypage.html", result1=result[0], result2= result[1],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning3="Please enter the correct name.")
+
+# rival's team
+
+# rival's team
+
+# rival's team
+    # query 4th pokemon
+    elif flask.request.method == 'POST' and flask.request.form.get('query4', None) == 'query':
+        pokemon_name = flask.request.form["Pokemon_name4"]
+        name = validName(pokemon_name)
+        if name != 0:
+            result4 = queryPokemon(name)
+            result[3]=result4
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
+        else:
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result5 = result[4], result6 = result[5],
+                                   warning4="Please enter the correct name.")
+    # query 5th pokemon
+    elif flask.request.method == 'POST' and flask.request.form.get('query5', None) == 'query':
+        pokemon_name = flask.request.form["Pokemon_name5"]
+        name = validName(pokemon_name)
+        if name != 0:
+            result5 = queryPokemon(name)
+            result[4]=result5
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
+        else:
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result6 = result[5],
+                                   warning5="Please enter the correct name.")
+    # query 6th pokemon
+    elif flask.request.method == 'POST' and flask.request.form.get('query6', None) == 'query':
+        pokemon_name = flask.request.form["Pokemon_name6"]
+        name = validName(pokemon_name)
+        if name != 0:
+            result6 = queryPokemon(name)
+            result[5] = result6
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5])
+        else:
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4],
+                                   warning6="Please enter the correct name.")
+
+    # add 4th pokemon
+    elif flask.request.method == 'POST' and flask.request.form.get('add4', None) == 'add':
+        pokemon_name = flask.request.form["Pokemon_name4"]
+        name = validName(pokemon_name)
+        if name != 0:
+            name_list[3] = (pokemon_name)
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   addation4="{} added".format(pokemon_name))
+        else:
+            return render_template("querypage.html", result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   warning4="Please enter the correct name.")
+    # add 5th pokrmon
+    elif flask.request.method == 'POST' and flask.request.form.get('add5', None) == 'add':
+        pokemon_name = flask.request.form["Pokemon_name5"]
+        name = validName(pokemon_name)
+        if name != 0:
+            name_list[4] = (pokemon_name)
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   addation5="{} added".format(pokemon_name))
+        else:
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result6 = result[5],
+                                   warning5="Please enter the correct name.")
+    # add 6th pokemon
+    elif flask.request.method == 'POST' and flask.request.form.get('add6', None) == 'add':
+        pokemon_name = flask.request.form["Pokemon_name6"]
+        name = validName(pokemon_name)
+        if name != 0:
+            name_list[5] = (pokemon_name)
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4], result6 = result[5],
+                                   addation6="{} added".format(pokemon_name))
+        else:
+            return render_template("querypage.html", result1=result[0], result2= result[1], result3=result[2],
+                                   result4=result[3], result5 = result[4],
+                                   warning6="Please enter the correct name.")
+
+
+
+
+
 
 
 if __name__ == '__main__':
